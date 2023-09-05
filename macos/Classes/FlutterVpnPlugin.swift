30ded60ad26d4ff84c369690537b22a1b2a324c5
amosogra <amosogra@gmail.com>
2023-09-05 07:01:34 +0100

    MacOS Implementation

--- macos/Classes/FlutterVpnPlugin.swift
+++ macos/Classes/FlutterVpnPlugin.swift
@@ -4,14 +4,39 @@ import FlutterMacOS
 public class FlutterVpnPlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
     let channel = FlutterMethodChannel(name: "flutter_vpn", binaryMessenger: registrar.messenger)
+    let stateChannel = FlutterEventChannel(name: "flutter_vpn_states", binaryMessenger: registrar.messenger)
+
     let instance = FlutterVpnPlugin()
     registrar.addMethodCallDelegate(instance, channel: channel)
+    stateChannel.setStreamHandler((VPNStateHandler() as! FlutterStreamHandler & NSObjectProtocol))
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
     switch call.method {
     case "getPlatformVersion":
       result("macOS " + ProcessInfo.processInfo.operatingSystemVersionString)
+      break
+    case "connect":
+      let args = call.arguments! as! [NSString: NSString]
+        VpnService.shared.connect(
+          result: result,
+          type: (args["Type"] as? String ?? "IKEv2"),
+          server: args["Server"]! as String,
+          username: args["Username"]! as String,
+          password: args["Password"]! as String,
+          secret: args["Secret"] as? String,
+          description: args["Name"] as? String
+        )
+      break
+    case "reconnect":
+      VpnService.shared.reconnect(result: result)
+      break
+    case "disconnect":
+      VpnService.shared.disconnect(result: result)
+      break
+    case "getCurrentState":
+      VpnService.shared.getState(result: result)
+      break   
     default:
       result(FlutterMethodNotImplemented)
     }
